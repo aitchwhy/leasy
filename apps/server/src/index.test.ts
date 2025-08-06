55a9debc28ecea93b2b63cdf9610fc17f34c7a88
Hank <hank.lee.qed@gmail.com>
2025-08-06 16:22:50 -0400

    bugfix: fix testing commands

--- apps/server/src/index.test.ts
+++ apps/server/src/index.test.ts
@@ -1,8 +1,9 @@
 import { describe, expect, it } from 'vitest'
+import app from './index'
 
-describe('index', () => {
-    it('should return 200 OK', async () => {
-        const res = await fetch('http://localhost:3000')
+describe('App', () => {
+    it('GET / returns 200 OK', async () => {
+        const res = await app.request('/')
         expect(res.ok).toBeTruthy()
         expect(await res.text()).toBe('OK')
     })
---
+++ vitest.config.ts
@@ -0,0 +1,17 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+    include: ['apps/**/*.test.ts', 'packages/**/*.test.ts'],
+    exclude: ['**/node_modules/**', '**/dist/**', 'tests/e2e/**']
+  },
+  resolve: {
+    alias: {
+      '@leasy/server': resolve(__dirname, './apps/server/src'),
+      '@leasy/shared': resolve(__dirname, './packages/shared/src')
+    }
+  }
+})
