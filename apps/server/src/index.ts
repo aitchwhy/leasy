0bd6fc69a1f8357a62052e7ee08fafff8f0d8d65
Hank <hank.lee.qed@gmail.com>
2025-08-10 20:32:42 -0400

    update

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,6 +2,22 @@ import { Hono } from "hono";
 import { ROOT_RESPONSE } from "./constants";
 import db from "./db.json";
 
+import { PostHog } from "posthog-node";
+
+const client = new PostHog("phc_qW23RO6Xu06MZkyQeJweNoyeEMLieSJrnVCfoY9bl7o", {
+  host: "https://us.i.posthog.com",
+});
+
+client.capture({
+  distinctId: "test-id",
+  event: "test-event",
+});
+
+// Send queued events immediately. Use for example in a serverless environment
+// where the program may terminate before everything is sent.
+// Use `client.flush()` instead if you still need to send more events or fetch feature flags.
+client.shutdown();
+
 const app = new Hono();
 
 // Health check
@@ -19,4 +35,7 @@ app.get("/buildings", (c) => {
   return c.json(db.units);
 });
 
-export default app;
+export default {
+  port: 4000,
+  fetch: app.fetch,
+};
